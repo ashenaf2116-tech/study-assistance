7e577674c85af14deb4f9cb902c52f65215253a8
ashenaf2116-tech <ashenaf2116@email.com>
2026-05-27 10:07:21 +0300

    add Task base class with encapsulation

---
+++ Task.java
@@ -0,0 +1,24 @@
+public class Task {
+
+    private String title;
+    private boolean isCompleted;
+
+    public Task(String title) {
+        this.title = title;
+        this.isCompleted = false;
+
+    }
+
+    public boolean isCompleted(){
+        return isCompleted;
+    }
+
+    public void markComplete() {
+        isCompleted = true;
+
+    }
+    public void show() {
+        System.out.println("Task:" + title + "Done:" + isCompleted);
+    }
+
+}
