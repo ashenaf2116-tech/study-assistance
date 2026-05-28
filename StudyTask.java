44afb818af5ac55a891bf6373507368fa1c87cee
dagmawimathewos-glitch <dagmawi.mathewos@bitscollege.edu.et>
2026-05-28 11:49:52 +0300

    StudyTask.java

--- StudyTask.java
+++ StudyTask.java
@@ -1,1 +1,18 @@
+public class StudyTask extends Task {
 
+    private String subject;
+
+    public StudyTask(String title, String subject) {
+        super(title);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public void show() {
+        System.out.println(" Study Task: " + getTitle() + " Subject: " + subject + " Done: " + isCompleted());
+    }
+}
